bb1cc39370f0cf1a14d66cc1aa98834988a0f0df
webnix-space <sanibmalik850@gmail.com>
2026-07-06 17:24:37 +0530

    update routes with layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
 import Home from './pages/Home.jsx'
 import SendTip from './pages/SendTip.jsx'
+import History from './pages/History.jsx'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white">
-      <nav className="border-b border-white/10 p-4">
-        <h1 className="text-xl font-bold text-green-400">ArcDrop</h1>
-      </nav>
-      <main className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/send" element={<SendTip />} />
-        </Routes>
-      </main>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/send" element={<SendTip />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </Layout>
   )
 }
 
